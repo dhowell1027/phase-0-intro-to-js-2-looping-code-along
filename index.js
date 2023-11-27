d1944d97cfd2b7082bcdd868a17e18bc871c3297
Dan <dhowell1027@yahoo.com>
2023-11-27 15:04:52 -0700

    looping practice

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Code your solutions in this file
+//for (let age = 30; age < 40; age++) {
+  //  console.log(`I'm ${age} years old. Happy birthday to me!`);
+    
+  //}
+  
+  //const gifts = ["teddy bear", "drone", "doll"];
+
+//function wrapGifts(gifts) {
+  //for (let i = 0; i < gifts.length; i++) {
+    //console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    //debugger;
+  //}
+
+  //return gifts;
+//}
+
+//wrapGifts(gifts);
+
+function writeCards(names, eventName) {
+    let thankYouMessages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      let message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+      thankYouMessages.push(message);
+    }
+  
+    return thankYouMessages;
+  }
+  
+  // Example usage:
+  const names = ["Charlie", "Samip", "Ali"];
+  const eventName = "birthday";
+  
+  const messages = writeCards(names, eventName);
+  console.log(messages);
+
+  function countDown(startingNumber) {
+    while (startingNumber >= 0) {
+      console.log(startingNumber);
+      startingNumber--;
+    }
+  }
+  countDown(10);
+  
